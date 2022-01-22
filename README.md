3db55538b5ffbbfce9d5e62d6dfd8f12fb16f090
w0lf3n <w0lf3n@coole-files.de>
2022-01-22 12:33:28 +0100

    get billboards method

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # chorus_analysis
+
+https://towardsdatascience.com/49-years-of-lyrics-why-so-angry-1adf0a3fa2b4?gi=68eb0f3be8c1
--- src/index.js
+++ src/index.js
@@ -1,42 +1,6 @@
 
-(function (from_year, to_year) {
+import {get_billboards_top_ten} from "./loader.js";
 
-    document.body.innerHTML = "";
 
-    const API_URL = "https://en.wikipedia.org/w/api.php?origin=*&action=parse&page=Billboard_Year-End_Hot_100_singles_of_%YEAR%&format=json";
-
-    const top_ten = {};
-
-    const fetch_top_ten_of_year = function (year) {
-
-        const wrapper = document.createElement("div");
-
-        return fetch(API_URL.replace(/%YEAR%/, year))
-            .then(response => response.json())
-            .then(result => {
-
-                wrapper.innerHTML = result.parse.text["*"];
-                const rows = wrapper.querySelectorAll("tbody tr");
-
-                top_ten[year] = [];
-
-                for (let i = 1; i < 11; i = i + 1) {
-                    const data = rows[i].textContent.split("\n");
-                    top_ten[year].push({
-                        position: data[1],
-                        title: data[2].replace(/"/g, ""),
-                        artist: data[3]
-                    });
-                }
-
-            });
-
-    };
-
-    const promises = [];
-    for (let i = from_year; i < to_year + 1; i = i + 1) {
-        promises.push(fetch_top_ten_of_year(String(i)));
-    }
-    Promise.all(promises).then(() => console.log(JSON.stringify(top_ten)));
-
-})(1980, 1989);
+const top_ten = get_billboards_top_ten(1970, 1979);
+document.write(JSON.stringify(top_ten));
